0b8b2093d15a00ce201fb5a8956e3ad42423fb18
Jeremy McLynch <admin@jmclynch.org>
2025-12-09 10:06:18 -0500

    46          2025-12-09 10:06:14  26.05.20251202.418468a  6.18.0  Unknown                 []              True

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -30,7 +30,7 @@
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
-    loader.timeout = 2;
+    loader.timeout = 0;
 
   };
 
