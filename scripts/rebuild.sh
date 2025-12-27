154edead0e9f53acb37498d40f35eb2e57bdc5ba
Jeremy McLynch <admin@jmclynch.org>
2025-12-27 04:46:19 -0500

    68          2025-12-22 14:59:26  26.05.20251202.418468a  6.18.0  Unknown                 []              True

---
+++ scripts/rebuild.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "$1" ]; then
+  echo "No Argument given, exiting..."
+  exit 1
+fi
+# cd to your config dir
+pushd ~/nix/
+
+# Early return if no changes were detected (thanks @singiamtel!)
+if [ -z "$(git diff)" ] && [ "$(git pull)" = "Already up to date." ]; then
+  echo "No changes detected, exiting."
+  popd
+  exit 0
+fi
+
+# Shows your changes
+git diff -U0
+
+echo "NixOS Rebuilding..."
+
+# Rebuild, output simplified errors, log trackebacks
+sudo nixos-rebuild switch --flake ~/nix/#"$1" &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+
+# Get current generation metadata
+current=$(nixos-rebuild list-generations | grep True)
+
+# Commit all changes witih the generation metadata
+git commit -am "$current"
+git push origin main || notify-send -e "NixOS Rebult, Git did not push to orgin main."
+# Back to where you were
+popd
+
+# Notify all OK!
+notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
