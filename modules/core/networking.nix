602efd0303eabb7842b969bd703cf4d33bba5ff0
Jeremy McLynch <admin@jmclynch.org>
2026-01-21 18:52:50 -0500

    74          2026-01-21 18:52:49  26.05.20251202.418468a  6.17.10  Unknown                 []              True

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -47,6 +47,10 @@
    #   "1.1.1.1#one.one.one.one"
    #   "1.0.0.1#one.one.one.one"
    # ];
+   extraConfig = ''
+    DNS=1.1.1.1#one.one.one.one
+    DNS=1.0.0.1#one.one.one.one
+  '';
     fallbackDns = [
       "1.1.1.1#one.one.one.one"
       "1.0.0.1#one.one.one.one"
