dcc4584dc5e7895e8f1f121b9266da7064954f1e
Jeremy McLynch <admin@jmclynch.org>
2026-02-03 16:50:56 -0500

    92          2026-02-03 16:50:53  26.05.20251202.418468a  6.17.10  Unknown                 []              True

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -40,6 +40,22 @@
   services.openssh.enable = true;
   services.upower.enable = true;
 
+
+# Use the GNOME Wayland session
+services.xserver.enable = true;
+services.xrdp.enable = true;
+services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
+
+# XRDP needs the GNOME remote desktop backend to function
+services.gnome.gnome-remote-desktop.enable = true;
+
+# Open the default RDP port (3389)
+services.xrdp.openFirewall = true;
+
+# Disable autologin to avoid session conflicts
+services.displayManager.autoLogin.enable = false;
+services.getty.autologinUser = null;
+
 services.udev = {
     packages = with pkgs; [
       qmk
