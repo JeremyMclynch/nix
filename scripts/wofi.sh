aefb5ecfdd8c4bfa6b6aff33f5f9d745064c2343
Jeremy McLynch <jmm277@njit.edu>
2026-02-13 08:34:25 -0500

    wofi

--- scripts/wofi.sh
+++ scripts/wofi.sh
@@ -11,9 +11,33 @@ else
 fi
 nvim="foot -e --title='rebuild' nvim"
 
-DISPLAY_NAMES=("host.nix" "home.nix" "pkg.nix" "config.fish" "hypr.conf" "hypr/rules.conf" "hypr/keybinds.conf" "Explore" "rebuild" "exit")
-COMMANDS=("$nvim ../hosts/${systemname}/default.nix" "$nvim ../home/jeremy/common.nix" "$nvim ../home/jeremy/packages.nix" "$nvim ../dots/fish/config.fish"
-  "$nvim ../dots/hypr/hyprland.conf" "$nvim ../dots/hypr/hyprland/rules-noctalia.conf" "$nvim ../dots/hypr/hyprland/keybinds-noctalia.conf" "$nvim ../" ":" "break")
+DISPLAY_NAMES=(
+  "host.nix"
+  "home.nix"
+  "pkg.nix"
+  "config.fish"
+  "hypr.conf"
+  "hypr/rules.conf"
+  "hypr/keybinds.conf"
+  "Explore"
+  "wofi.sh"
+  "hypr-dir"
+  "rebuild"
+  "exit")
+
+COMMANDS=(
+  "$nvim ../hosts/${systemname}/default.nix"
+  "$nvim ../home/jeremy/common.nix"
+  "$nvim ../home/jeremy/packages.nix"
+  "$nvim ../dots/fish/config.fish"
+  "$nvim ../dots/hypr/hyprland.conf"
+  "$nvim ../dots/hypr/hyprland/rules-noctalia.conf"
+  "$nvim ../dots/hypr/hyprland/keybinds-noctalia.conf"
+  "$nvim ../"
+  "$nvim ../scripts/wofi.sh"
+  "$nvim ../dots/hypr/hyprland/"
+  ":"
+  "break")
 
 # Use wofi in dmenu mode, passing the menu items via stdin
 # --normal-window is used for non-wayland environments, otherwise may not be needed
