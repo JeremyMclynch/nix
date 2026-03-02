8d8a501f8e219bf1a584763504bf95f298d1ade6
Jeremy McLynch <jmm277@njit.edu>
2026-03-02 00:32:51 -0500

    74          2026-03-02 00:32:47  26.05.20251202.418468a  6.17.10  Unknown                 []              True

--- modules/power/laptop-power.nix
+++ modules/power/laptop-power.nix
@@ -1,6 +1,19 @@
 
-{ ... }:
+{ pkgs, ... }:
 {
   # GNOME-friendly power mode switching
   services.power-profiles-daemon.enable = true;
+
+  # Set brightness to 100% on boot
+  systemd.services.set-brightness = {
+    description = "Set screen brightness to 75% on boot";
+    wantedBy = [ "multi-user.target" ]; # Or graphical.target
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.brightnessctl}/bin/brightnessctl set 75%";
+    };
+  };
+
+  # Ensure brightnessctl is available
+  environment.systemPackages = [ pkgs.brightnessctl ];
 }
