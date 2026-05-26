aa38fce2fe921fa3dde9a1ce09265ccf71b88440
Jeremy McLynch <admin@jmclynch.org>
2026-05-25 20:41:49 -0400

    142         2026-05-25 00:24:09  26.05.20260510.da5ad66  7.0.5-zen1    Unknown                 []              True

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -71,7 +71,7 @@
 
   environment.variables = {
     GDK_SCALE = "1";
-    AQ_DRM_DEVICES = "/dev/dri/card2";
+#    AQ_DRM_DEVICES = "/dev/dri/card2";
   };
 
   services.flatpak.enable = true;
@@ -92,21 +92,21 @@
     capSysNice = false;
   };
 
-  services.displayManager.sessionPackages = [
-    (pkgs.runCommand "gamescope-session" {
-      passthru.providedSessions = [ "gamescope-session" ];
-    } ''
-      mkdir -p $out/share/wayland-sessions
-      cat > $out/share/wayland-sessions/gamescope-session.desktop <<EOF
-      [Desktop Entry]
-      Name=Gamescope (HDR Gaming)
-      Comment=Steam Big Picture on OLED via dGPU
-      Exec=env WLR_DRM_DEVICES=/dev/dri/card1 MESA_VK_DEVICE_SELECT=1002:744c gamescope --backend drm --prefer-output DP-1 -W 2560 -H 1440 -r 240 --hdr-enabled --adaptive-sync --steam --expose-wayland --force-grab-cursor -- steam -tenfoot -gamepadui -steamos3 -steampal
-      Type=Application
-      DesktopNames=gamescope
-      EOF
-    '')
-  ];
+#  services.displayManager.sessionPackages = [
+#    (pkgs.runCommand "gamescope-session" {
+#      passthru.providedSessions = [ "gamescope-session" ];
+#    } ''
+#      mkdir -p $out/share/wayland-sessions
+#      cat > $out/share/wayland-sessions/gamescope-session.desktop <<EOF
+#      [Desktop Entry]
+#      Name=Gamescope (HDR Gaming)
+#      Comment=Steam Big Picture on OLED via dGPU
+#      Exec=env WLR_DRM_DEVICES=/dev/dri/card1 MESA_VK_DEVICE_SELECT=1002:744c gamescope --backend drm --prefer-output DP-1 -W 2560 -H 1440 -r 240 --hdr-enabled --adaptive-sync --steam --expose-wayland --force-grab-cursor -- steam -tenfoot -gamepadui -steamos3 -steampal
+#      Type=Application
+#      DesktopNames=gamescope
+#      EOF
+#    '')
+#  ];
 
 systemd.user.services.capture-card-loopback = {
   description = "Loopback Elgato capture card audio to FiiO K3";
