3d3d981e95e74d5c46ab2fc3d6d7e2ff68d4e949
Jeremy McLynch <admin@jmclynch.org>
2026-06-17 13:23:31 -0400

    166         2026-06-17 13:23:29  26.11.20260610.9ae611a  7.0.10-zen1   Unknown                 []              True

--- dots/hypr/scripts/wsaction.fish
+++ dots/hypr/scripts/wsaction.fish
@@ -1,8 +1,12 @@
 #!/usr/bin/env fish
 
+# Switch to / move a window to a workspace, scoped to the current workspace
+# "group" (blocks of 10). Under Hyprland 0.55+ Lua configs, `hyprctl dispatch`
+# parses its argument as Lua, so dispatchers must be passed as hl.dsp.* calls.
+
 if test "$argv[1]" = '-g'
     set group
-    set -e $argv[1]
+    set -e argv[1]
 end
 
 if test (count $argv) -ne 2
@@ -12,10 +16,20 @@ end
 
 set -l active_ws (hyprctl activeworkspace -j | jq -r '.id')
 
+set -l target
 if set -q group
     # Move to group
-    hyprctl dispatch $argv[1] (math "($argv[2] - 1) * 10 + $active_ws % 10")
+    set target (math "($argv[2] - 1) * 10 + $active_ws % 10")
 else
     # Move to ws in group
-    hyprctl dispatch $argv[1] (math "floor(($active_ws - 1) / 10) * 10 + $argv[2]")
+    set target (math "floor(($active_ws - 1) / 10) * 10 + $argv[2]")
+end
+
+switch $argv[1]
+    case workspace
+        hyprctl dispatch "hl.dsp.focus({ workspace = \"$target\" })"
+    case movetoworkspace
+        hyprctl dispatch "hl.dsp.window.move({ workspace = \"$target\" })"
+    case '*'
+        hyprctl dispatch "hl.dsp.$argv[1]({ workspace = \"$target\" })"
 end
